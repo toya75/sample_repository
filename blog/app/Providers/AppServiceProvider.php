63ed6fe0cee5a4efeba37f39f27a5acb1f08e194
toya75 <k601493@kansai-u.ac,jp>
2024-07-20 11:09:25 +0900

    適切なコミットメッセージ

---
+++ blog/app/Providers/AppServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+       Paginator::useBootstrap();    
+    }
+}
